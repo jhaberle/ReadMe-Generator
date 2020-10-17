4cf30550480c87c38884e1195ade1f434a7e6f45
Jake Haberle <jake.haberle@gmail.com>
2020-10-17 11:45:21 -0600

    added questions to array

--- Develop/index.js
+++ Develop/index.js
@@ -6,7 +6,57 @@ const generateMarkdown = ("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+    type: "input",
+    message: "What is the title of the project?",
+    name: "title"
+    },
+    {
+        type: "input",
+        message: "Write a brief description of the project",
+        name: "description"
+    },
+    {
+        type: "input",
+    message: "What are the installation instructions?",
+    name: "install"
+    },
+    {
+        type: "input",
+        message: "What is the usage of the application?",
+        name: "usage"  
+    },
+    {
+        type: "input",
+        message: "Are there any contribution guidelines?",
+        name: "contribution" 
+    },
+    {
+        type: "input",
+        message: "What are the test instructions?",
+        name: "test"   
+    },
+    {
+        type: "list",
+        message: "Is there a license preference for the project?",
+        name: "license",
+        choices: [
+          "ISC", 
+          "GNU", 
+          "MIT", 
+          "Apache"
+        ]
+    },
+    {
+        type: "input",
+        message: "Enter github user name",
+        name: "github" 
+    },
+    {
+        type: "input",
+        message: "Enter email address",
+        name: "email"
+    }
 ];
 
 // function to write README file
